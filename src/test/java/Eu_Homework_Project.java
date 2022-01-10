baca10d12a19e10a1a063fdcbb7762b38897da2b
Okuducu4609 <okuducu4609@gmail.com>
2022-01-10 20:26:29 +0100

    Grubproject  performed

---
+++ src/test/java/Eu_Homework_Project.java
@@ -0,0 +1,54 @@
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+import java.util.concurrent.TimeUnit;
+
+
+
+/*Test case #1
+Step 1. Go to “https://practicecybertekschool.herokuapp.com”
+Step 2. Click on “Registration Form”
+Step 3. Enter “wrong_dob” into date of birth input box.
+Step 4. Verify that warning message is displayed: “The date of birth is not valid”
+
+Test case #2
+  Step 1. Go to “https://practicecybertekschool.herokuapp.com”
+  Step 2. Click on “Registration Form”
+  Step 3. Verify that following options for programming languages are displayed: c++, java, JavaScript */
+
+public class Eu_Homework_Project {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    Faker faker=new Faker();
+
+    @BeforeMethod
+
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void Test1() {
+
+
+
+
+
+
+    }
+}
